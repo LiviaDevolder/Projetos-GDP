ecc71eb7a79131df3aa5787f232e98f4723b42be
LiviaDevolder <liviadevolder@gmail.com>
2021-06-28 21:25:17 -0300

    Aula 50

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
     const urlencodedParse = bodyParser.urlencoded({ extended: false })
     const session = require('express-session')
     const flash = require('connect-flash')
+    require("./models/Postagem")
+    const Postagem = mongoose.model("postagens")
 
 // Configurações
     // Sessão
@@ -46,7 +48,16 @@
 
 // Rotas
     app.get('/', (req, res) => {
-        res.send('Rota principal')
+        Postagem.find().lean().populate("categoria").sort({data: "desc"}).then((postagens) => {
+            res.render("index", {postagens: postagens})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/404")
+        })
+    })
+
+    app.get("/404", (req, res) => {
+        res.send('Erro 404!')
     })
 
     app.get('/posts', (req, res) => {
